b8f4b6514c6d7b167c2c877dad75a82a40e3a1f4
Princeop12 <93066124+Princeop12@users.noreply.github.com>
2022-06-06 11:20:21 +0530

    Update play.py

--- handlers/play.py
+++ handlers/play.py
@@ -165,7 +165,7 @@ async def play(_, message: Message):
 
         file_name = get_file_name(audio)
         title = file_name
-        thumb_name = "https://telegra.ph/file/9350788513346feef5087.jpg"
+        thumb_name = "https://telegra.ph/file/3a25e361a2c24a50535d5.jpg"
         thumbnail = thumb_name
         duration = round(audio.duration / 60)
         views = "Locally added"
@@ -232,7 +232,7 @@ async def play(_, message: Message):
     
         except Exception as e:
             title = "NaN"
-            thumb_name = "https://telegra.ph/file/9350788513346feef5087.jpg"
+            thumb_name = "https://telegra.ph/file/3a25e361a2c24a50535d5.jpg"
             duration = "NaN"
             views = "NaN"
             keyboard = InlineKeyboardMarkup(
