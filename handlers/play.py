670046df1e8962290e3c5dd1d06e2acbe3330829
Princeop12 <93066124+Princeop12@users.noreply.github.com>
2022-06-06 11:53:21 +0530

    Update play.py

--- handlers/play.py
+++ handlers/play.py
@@ -177,7 +177,7 @@ async def play(_, message: Message):
                             text="✨ᴏᴡɴᴇʀ✨",
                             url=f"https://t.me/ROCKSTAR_PRINCE_OP"),
                     InlineKeyboardButton(
-                            text="ᴜᴘᴅᴀᴛᴇs👾",
+                            text="ᴀᴅᴅ ᴍᴇ🔥",
                             url=f"http://t.me/{BOT_USERNAME}?startgroup=true"),
                ],
                [
@@ -221,7 +221,7 @@ async def play(_, message: Message):
                             text="✨ᴏᴡɴᴇʀ✨",
                             url=f"https://t.me/ROCKSTAR_PRINCE_OP"),
                     InlineKeyboardButton(
-                            text="ᴜᴘᴅᴀᴛᴇs👾",
+                            text="ᴀᴅᴅ ᴍᴇ🔥",
                             url=f"http://t.me/{BOT_USERNAME}?startgroup=true"),
                ],
                [
@@ -242,7 +242,7 @@ async def play(_, message: Message):
                             text="✨ᴏᴡɴᴇʀ✨",
                             url=f"https://t.me/ROCKSTAR_PRINCE_OP"),
                     InlineKeyboardButton(
-                            text="ᴜᴘᴅᴀᴛᴇs👾",
+                            text="ᴀᴅᴅ ᴍᴇ🔥",
                             url=f"http://t.me/{BOT_USERNAME}?startgroup=true"),
                ],
                [
@@ -301,7 +301,7 @@ async def play(_, message: Message):
                             text="✨ᴏᴡɴᴇʀ✨",
                             url=f"https://t.me/ROCKSTAR_PRINCE_OP"),
                     InlineKeyboardButton(
-                            text="ᴜᴘᴅᴀᴛᴇs👾",
+                            text="ᴀᴅᴅ ᴍᴇ🔥",
                             url=f"http://t.me/{BOT_USERNAME}?startgroup=true"),
                ],
                [
